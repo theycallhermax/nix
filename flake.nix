4bb9df5fe842b3f32fc2847b6e6390daefd70d12
theycallhermax <mdwalters.pm@proton.me>
2025-03-30 11:28:40 -0400

    (hopefully) add zen browser

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
 			url = "github:nix-community/home-manager/release-24.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
+		zen-browser = {
+			url = "github:youwen5/zen-browser-flake";
+			inputs.nixpkgs.follows = "nixpkgs";
+		}
 	};
 
 	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
