8abee1dbb7fb9fa7717e257f3a5744801b030f7f
theycallhermax <mdwalters.pm@proton.me>
2025-03-30 06:57:03 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -7,17 +7,19 @@
       			./hardware-configuration.nix
     		];
 
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
-	networking.hostName = "shitfuck";
+	networking.hostName = "supabema";
 	networking.networkmanager.enable = true;
 
 	time.timeZone = "America/Toronto";
 	i18n.defaultLocale = "en_CA.UTF-8";
 
 	services.xserver.enable = true;
-	services.xserver.libinput.enable = true;
+	services.libinput.enable = true;
 	services.xserver.displayManager.gdm.enable = true;
 	services.xserver.desktopManager.gnome.enable = true;
 	services.xserver.xkb = {
@@ -25,12 +27,9 @@
 		variant = "";
   	};
 	environment.gnome.excludePackages = with pkgs; [
-		nixos-background-info
+		# nixos-background-info
 		gnome-tour
 		geary
-	];
-	environment.systemPackages = with pkgs.gnomeExtensions; [
-		blur-my-shell
 	];
 
 	services.printing.enable = true;
@@ -48,42 +47,23 @@
 		isNormalUser = true;
 		description = "Max Lunaris";
 		extraGroups = [ "networkmanager" "wheel" ];
+		hashedPassword = "$6$xnOj1Mi.Lc1mQZ2a$xyNrOr4nMGUNT6fPjJPXkHEgV6Atw15dfcq7.Tj6YpFCHF1IJPYFj7shNSdDk9BAkbqLNZoFMapN/7N5Vm4sK/";
 		packages = with pkgs; [
-    			#  thunderbird
-    		];
+			fastfetch
+		];
 	};
 
 	nixpkgs.config.allowUnfree = true;
 
-	environment.systemPackages = with pkgs; [
-		vim 
+	environment.systemPackages = [
+		pkgs.gnomeExtensions.blur-my-shell
+		pkgs.gnomeExtensions.just-perfection
+		pkgs.gnomeExtensions.dash-to-dock
+		pkgs.vim
+		pkgs.git
 	];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+	system.stateVersion = "24.11";
 
 }
+
